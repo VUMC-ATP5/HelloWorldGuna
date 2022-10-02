d31dbd8348f97e6bce6c8ddd8259b994cba425b4
GunaVendina <gvendina@inbox.lv>
2022-10-02 18:55:20 +0300

    Update FirstClass.java
    Mājasdarbs pabeigts

--- src/main/java/lekcijaViens/majasdarbs/FirstClass.java
+++ src/main/java/lekcijaViens/majasdarbs/FirstClass.java
@@ -4,28 +4,18 @@ public class FirstClass {
     public static void main(String[] args) {
 
         /*    1. Jums ir nepieciešams izveidot  jaunu Java klasi ar nosaukumu ‘FirstClass.java’ zem homework mapes.
-
     2. FirstClass.java failā jāizveido ‘main’ metodi
-
     3. Main metodē vajag nodefinēt mainīgos (ar atbilstošo datatipu) Valstij (brīva izvēle) un izvadīt uz ekrāna:
-
         1. Nosaukums
-
         2. Iedzīvotājus skaits
-
         3. Platība
-
         4. Galvaspilsēta
-
         5. Oficiāla valoda
-
         6. Vai ir ES dalībvalsts
-
         7. Valūta (viens simbols)
-
     4. Jāuzraksta dažus teikumus par brīvu tēmu izmantojot konkatenāciju
-
-    5. Jāuzraksta pa trīs piemēriem uz katru aritmētisko operatoru.*/
+    5. Jāuzraksta pa trīs piemēriem uz katru aritmētisko operatoru.
+    */
 
     String country;
     long population;
@@ -57,5 +47,49 @@ public class FirstClass {
         System.out.println("Vai ir ES dalībvalsts: " + isEUCountry);
         System.out.println("Valūtas: " + currency1 + ", " + currency2);
 
+
+    //4.uzdevuma izpilde
+
+    String continent;
+    String borderingCountry1;
+    String borderingCountry2;
+    short numberOfWives;
+    short numberOfChildren;
+
+        continent = "Afrika";
+        borderingCountry1 = "South Africa";
+        borderingCountry2 = "Mozambique";
+        numberOfWives = 15;
+        numberOfChildren = 36;
+
+        System.out.println("     " + country + ", officially the Kingdom of Eswatini and formerly named Swaziland is a landlocked country in " +continent +".\nIt is bordered by " + borderingCountry1 + " and " + borderingCountry2 + ".\nAt no more than 200 kilometres north to south and 130 kilometres east to west,\nEswatini is one of the smallest countries in " + continent + ".");
+        System.out.println("     The king of " + country + " is Mswati III. He was crowned on 25 April 1986 at the age of 18, \nthus becoming the youngest ruling monarch in the world at that time. \nTogether with his mother he rules the country as an absolute monarch. \nMswati III is known for his practice of polygamy and currently has " + numberOfWives + " wives and " + numberOfChildren + " children.");
+
+    //5.uzdevuma izpilde (nav gluži pa 3 piemēriem no katras darbības, bet princips skaidrs)
+
+     int length = 10;
+     int width = 8;
+     double radius = 7.7;
+     double pi = 3.14;
+     double distance = 100.7;
+     double time = 0.8;
+     int x = 17;
+     int y = 5;
+
+        int perimeter=2*(length+ width);
+        int areaOfRectangle = length* width;
+        double circleLineLength= 2*pi*radius;
+        double circleArea = pi*radius*radius;
+        double speed = distance / time;
+        double sum = x+y;
+        double residual = x-y;
+        double multipl = x*y;
+        double div = x/y;
+        double modulo = x%y;
+
+        System.out.println("\nJa taisnstūra garums ir " + length + "m un platums ir " + width + "m, \ntad taisnstūra perimetrs ir " + perimeter + "m, bet laukums ir " + areaOfRectangle + "m.");
+        System.out.println("\nJa riņķa līnijas rādiuss ir " + radius + "cm, \ntad riņķa līnijas garums ir " + circleLineLength + "cm, bet riņķa laukums ir " + circleArea + "cm2.");
+        System.out.println("\nJa ķermenis " + distance + "km veic " + time + " stundās, \ntad tā kustības ātrums ir " + speed + "km/h.");
+        System.out.println("\nSkaitļu " + x + " un " + y + " summa ir " + sum + ",\nstarpība ir " + residual + ",\nreizinājums ir " + multipl + ",\ndalījums ir " + div + ",\nbet dalijuma atlikums (modulo) ir " + modulo + ".");
     }
 }
