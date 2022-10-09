109c960e028e7ca3cc57e5567cad56cbdf13f815
GunaVendina <gvendina@inbox.lv>
2022-10-09 14:06:19 +0300

    Update labDarbsDivi.java
    papildināts

--- src/main/java/lekcijaDivi/labDarbs/labDarbsDivi.java
+++ src/main/java/lekcijaDivi/labDarbs/labDarbsDivi.java
@@ -135,7 +135,7 @@ public class labDarbsDivi {
             } else {
                 System.out.println("Skaitlis " + skaitlisIevaditais + " ir negatīvs skaitlis.");
             }
-                
+
             }
         }
 
